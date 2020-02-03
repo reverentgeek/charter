a9071bdb0f6737cd99b82b743e43e63fbfabad3f
David Neal <david@reverentgeek.com>
2020-02-03 15:15:55 -0500

    adds rendering to pdf with wkhtmltopdf

--- index.js
+++ index.js
@@ -1,37 +1,64 @@
 "use strict";
 
 const chordpro = require( "chordprojs" );
-const fs = require( "fs" );
+const fs = require( "fs" ).promises;
 const path = require( "path" );
 const hb = require( "handlebars" );
-let __template;
+const handler = require( "serve-handler" );
+const http = require( "http" );
+const util = require( "util" );
+const exec = util.promisify( require( "child_process" ).exec );
+const port = 3000;
 
-const getChartTemplate = () => {
-	if ( __template ) {
-		return __template;
+async function renderPdf( port, name ) {
+	const { stderr } = await exec( `wkhtmltopdf http://localhost:${ port }/charts/${ name } ./charts/${ name }.pdf` );
+	if ( stderr ) {
+		console.log( stderr );
 	}
-	const text = fs.readFileSync( path.join( __dirname, "chart.hbs" ), "utf8" );
-	__template = hb.compile( text );
-	return __template;
-};
+}
 
-const parseChordFile = ( chordFile, htmlFile ) => {
-	const text = fs.readFileSync( chordFile, "utf8" );
+async function getChartTemplate() {
+	const text = await fs.readFile( path.join( __dirname, "chart.hbs" ), "utf8" );
+	const template = hb.compile( text );
+	return template;
+}
+
+async function parseChordFile( template, chordFile, htmlFile ) {
+	const text = await fs.readFile( chordFile, "utf8" );
 	const song = chordpro.parse( text );
 	const chart = song.render();
-	const template = getChartTemplate();
 	const html = template( { body: chart } );
-	fs.writeFileSync( htmlFile, html, { encoding: "utf8" } );
-};
+	await fs.writeFile( htmlFile, html, { encoding: "utf8" } );
+}
 
-const chartFolder = path.join( __dirname, "charts" );
-const files = fs.readdirSync( chartFolder );
-const chordFiles = files.filter( f => f.endsWith( ".cho" ) || f.endsWith( ".chordpro" ) );
-chordFiles.forEach( cf => {
-	const fileName = path.basename( cf, cf.endsWith( ".cho" ) ? ".cho" : ".chordpro" );
-	const filePath = path.join( chartFolder, cf );
+async function renderChart( chordFile, chartFolder, template ) {
+	const fileName = path.basename( chordFile, chordFile.endsWith( ".cho" ) ? ".cho" : ".chordpro" );
+	const filePath = path.join( chartFolder, chordFile );
 	const newFile = path.join( chartFolder, `${ fileName }.html` );
 	console.log( filePath );
 	console.log( newFile );
-	parseChordFile( filePath, newFile );
+	await parseChordFile( template, filePath, newFile );
+	await renderPdf( port, fileName );
+}
+
+async function renderCharts() {
+	const template = await getChartTemplate();
+	const chartFolder = path.join( __dirname, "charts" );
+	const files = await fs.readdir( chartFolder );
+	const chordFiles = files.filter( f => f.endsWith( ".cho" ) || f.endsWith( ".chordpro" ) );
+	for( let i = 0; i < chordFiles.length; i++ ){
+		await renderChart( chordFiles[i], chartFolder, template );
+	}
+}
+
+const server = http.createServer( ( request, response ) => {
+	// You pass two more arguments for config and middleware
+	// More details here: https://github.com/zeit/serve-handler#options
+	return handler( request, response );
+} );
+
+server.listen( 3000, async () => {
+	await renderCharts();
+	server.close();
+	console.log( "finished" );
 } );
--- readme.md
+++ readme.md
@@ -1,8 +1,26 @@
 # Charter
 
-Note: _Work in Progress_
+> Note: _Work in Progress_
 
-Turns `.chordpro` files into formatted HTML chord charts.
+Turns `.chordpro` files into formatted HTML chord charts and renders them to PDF.
+
+## Setup
+
+To convert HTML files to PDF, you will need to [wkhtmltopdf](https://wkhtmltopdf.org/) installed on your system. For macOS, you can install this with [Homebrew](https://brew.sh/) using:
+
+```sh
+brew cask install wkhtmltopdf
+```
+
+### Install Dependencies
+
+Before you can use the source code, you will need to install dependencies. From the source code folder, run the following command.
+
+```sh
+npm install
+```
+
+## Usage
 
 Put your `.chordpro` files in the `charts` folder and run in your terminal:
 
@@ -10,4 +28,4 @@ Put your `.chordpro` files in the `charts` folder and run in your terminal:
 node .
 ```
 
-HTML files will be generated in the same `charts` folder.
+HTML and PDF files will be generated in the same `charts` folder.
